850cf470f1054622e66b6d12212b9e0729e6ea10
Emina Palamarevic <minapala342@gmail.com>
2020-06-13 12:13:06 -0400

    Delete and Edit

--- ResturantRater/Controllers/RestaurantController.cs
+++ ResturantRater/Controllers/RestaurantController.cs
@@ -1,7 +1,9 @@
 ﻿using ResturantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -38,5 +40,63 @@ namespace ResturantRater.Controllers
             }
             return View(restaurant);
         }
+
+        //Get Restaurant/Delete/ID
+        public ActionResult Delete(int? id)  //? id can now be null
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        //Get: Restaurant/Edit/id
+        //Get ID from User
+        //If ID is null //if no ID- get 400 bad request error
+        public ActionResult Edit (int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+
     }
+
 }
