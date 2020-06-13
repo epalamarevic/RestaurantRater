ef567f0f949dd1d920a46912e868945dda364e37
Emina Palamarevic <minapala342@gmail.com>
2020-06-13 12:31:46 -0400

    Details

--- ResturantRater/Controllers/RestaurantController.cs
+++ ResturantRater/Controllers/RestaurantController.cs
@@ -97,6 +97,21 @@ namespace ResturantRater.Controllers
             return View(restaurant);
         }
 
+        //Get Restaurant/details/ID
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
     }
 
 }
