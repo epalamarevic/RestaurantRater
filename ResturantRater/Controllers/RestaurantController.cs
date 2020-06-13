6dffd0d60896ef086bd45b07e0f7d38df23aa5f9
Emina Palamarevic <minapala342@gmail.com>
2020-06-13 10:50:47 -0400

    Index and Create

---
+++ ResturantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,42 @@
+﻿using ResturantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResturantRater.Controllers
+{
+    
+                //this is what the URL will be seen- Restaurant
+    public class RestaurantController : Controller
+    {
+        private RestaurantDbContext _db = new RestaurantDbContext();
+        // GET: Restaurant/index
+        public ActionResult Index()  //index actin for our view
+        {
+            return View(_db.Restaurants.ToList());
+        }
+
+
+        //Get Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Post:Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+    }
+}
---
+++ ResturantRater/Models/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ResturantRater.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; } //a table with all of our restaurants on it.
+        //i want a new collection of restaurant objects called Restaurants
+
+        //entity- data level model - get stored in our database
+        //model - 
+    }
+}
